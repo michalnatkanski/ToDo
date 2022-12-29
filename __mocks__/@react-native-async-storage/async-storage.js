f72226066fa0351ba21ba11a16d26685e29103ff
mnatkanski <m.natkanski0@gmail.com>
2022-12-29 17:39:04 +0100

    Add mock to redux persist for test testing

--- __mocks__/@react-native-async-storage/async-storage.js
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -1,1 +1,11 @@
 export default from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('redux-persist', () => {
+    const real = jest.requireActual('redux-persist');
+    return {
+        ...real,
+        persistReducer: jest
+            .fn()
+            .mockImplementation((config, reducers) => reducers),
+    };
+});
